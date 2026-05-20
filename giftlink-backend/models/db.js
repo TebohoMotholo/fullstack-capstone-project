e3815bbe160e5fd9e5c6b7c65c1140ff5513c24e
TebohoMotholo <motholoteboho@yahoo.com>
2026-05-19 20:20:01 -0400

    Update

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,26 @@
-// db.js
+/*jshint esversion: 8 */
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-
-// MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+const { MongoClient } = require('mongodb');
 
 let dbInstance = null;
-const dbName = "giftdb";
+const dbName = "giftDB";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    if (dbInstance) {
+        return dbInstance;
+    }
 
-    const client = new MongoClient(url);      
+    const mongoURI = process.env.MONGO_URL;
+    const client = new MongoClient(mongoURI);
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    // Task 2: Connect to database giftDB and store in dbInstance
+    dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+    // Task 3: Return the database instance
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
